ae33d345e5dc8397cdd10e65e6b79e820d252cf3
SvenRandom <yb576203915@163.com>
2018-05-02 04:57:01 +1000

    media for new diet
    1. add taking photo function and save it to Albm.
    2. add pick photo function from device Albm

--- MyDiet/Views/DietItemPage.xaml.cs
+++ MyDiet/Views/DietItemPage.xaml.cs
@@ -4,6 +4,9 @@ using MyDiet.Manager;
 using MyDiet.Models;
 using MyDiet.Helpers;
 using Xamarin.Forms;
+using Plugin.Media;
+using ZXing.Mobile;
+using Plugin.Media.Abstractions;
 
 namespace MyDiet.Views
 {
@@ -13,6 +16,8 @@ namespace MyDiet.Views
 		bool isNewItem = false;
         public static DietManager dietManager { get; private set; }
         DietItem dietItemCurrent;
+		private int numberOfPhoto = 0;
+        private String[] imagesPath = new string[3];
 
         public DietItemPage(DietItem dietItem)
         {
@@ -73,5 +78,156 @@ namespace MyDiet.Views
        
 
 
+        async void takePhotoClicked(object sender, System.EventArgs e)
+        {
+            if (numberOfPhoto == 3)
+            {
+                await DisplayAlert("Notice!", ":( at most three photos.", "OK");
+                return;
+            }
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+				PhotoSize = PhotoSize.Medium,
+				CompressionQuality = 92,
+				SaveToAlbum = true,
+                Name = "meal.jpg"
+
+            });
+
+            if (file == null)
+                return;
+
+			//await DisplayAlert("File Location", file.AlbumPath, "OK");
+
+           
+
+            switch (numberOfPhoto)
+            {
+                case 0:
+                    image1.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = file.GetStream();
+                        return stream;
+                    });
+                    imagesPath[0] = file.Path;
+                    break;
+                case 1:
+                    image2.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = file.GetStream();
+                        return stream;
+                    });
+                    imagesPath[1] = file.Path;
+                    break;
+                case 2:
+                    image3.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = file.GetStream();
+                        return stream;
+                    });
+                    imagesPath[2] = file.Path;
+                    break;
+                default:
+
+                    break;
+            }
+            numberOfPhoto++;
+			imagesStack.IsVisible = true;
+        }
+
+		async void pickPhotoClicked(object sender, System.EventArgs e)
+        {
+            if (numberOfPhoto == 3)
+            {
+                await DisplayAlert("Notice!", ":( at most three photos.", "OK");
+                return;
+            }
+            await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not available", ":( pick photo is not available.", "OK");
+                return;
+            }
+            var photos = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+
+            });
+
+            switch (numberOfPhoto)
+            {
+                case 0:
+                    image1.Source = ImageSource.FromStream(() =>
+                    {
+						var stream = photos.GetStream();
+                        return stream;
+                    });
+					imagesPath[0] = photos.Path;
+                    break;
+                case 1:
+                    image2.Source = ImageSource.FromStream(() =>
+                    {
+						var stream = photos.GetStream();
+                        return stream;
+                    });
+					imagesPath[1] = photos.Path;
+                    break;
+                case 2:
+                    image3.Source = ImageSource.FromStream(() =>
+                    {
+						var stream = photos.GetStream();
+                        return stream;
+                    });
+					imagesPath[2] = photos.Path;
+                    break;
+                default:
+
+                    break;
+            }
+            numberOfPhoto++;
+			imagesStack.IsVisible = true;
+
+        }
+
+        async void scanClicked(object sender, System.EventArgs e)
+        {
+            //MobileBarcodeScanner.Initialize(Application);
+
+#if __ANDROID__
+    // Initialize the scanner first so it can track the current context
+            MobileBarcodeScanner.Initialize (Application);
+#endif
+            // Initialize the scanner first so it can track the current context
+            //if (Device.RuntimePlatform == Device.Android)
+            //        {
+            //ZXing.Mobile.MobileBarcodeScanner.Initialize(Application);
+            //}
+
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            try
+            {
+
+                var result = await scanner.Scan();
+                bar.Text = result.Text;
+            }
+            catch
+            {
+                bar.Text = "no result";
+            }
+
+
+        }
+
+         
     }
 }
